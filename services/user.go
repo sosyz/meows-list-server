b117fe920be2cfa593c29ecc85fd8d61d6e8de41
Sonui <me@sonui.cn>
2022-08-22 22:40:25 +0800

    feat: user login #2

--- services/user.go
+++ services/user.go
@@ -3,17 +3,28 @@ package services
 import (
 	"errors"
 	"sonui.cn/meows-list-server/models"
+	"sonui.cn/meows-list-server/pkg/cache"
+	"sonui.cn/meows-list-server/pkg/crypto"
 )
 
-func UserLogin(email, password string) error {
+func UserLogin(email, password string) (string, error) {
 	user, err := models.GetUserByEmail(email)
 	if err != nil {
-		return errors.New("邮箱或密码错误")
+		return "", errors.New("邮箱或密码错误")
 	}
-	if !models.CheckUserPassword(user, password) {
-		return errors.New("邮箱或密码错误")
+	if !crypto.CheckPassword(password, user.Password) {
+		return "", errors.New("邮箱或密码错误")
 	}
-	return nil
+
+	token, err := crypto.RandString(64)
+	if err != nil {
+		return "", errors.New("登录失败")
+	}
+
+	if err := cache.Put(token, user, 0); err != nil {
+		return "", errors.New("登录失败")
+	}
+	return token, nil
 }
 
 func UserSignup(name, password, email, phone string) error {
@@ -24,7 +35,7 @@ func UserSignup(name, password, email, phone string) error {
 	if user.ID > 0 {
 		return errors.New("邮箱已被注册")
 	}
-	if err := models.SignUp(name, password, email, phone); err != nil {
+	if err := models.CreateUser(name, password, email, phone); err != nil {
 		return err
 	}
 	return nil
