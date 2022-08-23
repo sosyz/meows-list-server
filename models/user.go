534910d2255c93f41626c705959618e61596bb63
Sonui <me@sonui.cn>
2022-08-23 23:22:08 +0800

    feat: GetUserByID UpdateUser

--- models/user.go
+++ models/user.go
@@ -17,18 +17,26 @@ const (
 )
 
 type User struct {
-	gorm.Model `json:"-"`
-	Name       string
-	Level      int
-	Email      string
-	Password   string `json:"-"`
-	Phone      string
-	Status     int `json:"-"`
+	gorm.Model
+	Name     string
+	Level    int
+	Email    string
+	Password string
+	Phone    string
+	Status   int
 }
 
 func GetUserByEmail(email string) (*User, error) {
 	user := User{}
 	if err := DB.Where("email = ?", email).Limit(1).Find(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUserByID(id uint) (*User, error) {
+	user := User{}
+	if err := DB.Where("id = ?", id).Limit(1).Find(&user).Error; err != nil {
 		return nil, err
 	}
 	return &user, nil
@@ -49,4 +57,28 @@ func CreateUser(name, password, email, phone string) error {
 	}
 	user.Password = string(pw)
 	return DB.Create(&user).Error
+}
+
+func UpdateUser(id uint, name, email, phone, password string) error {
+	user := User{}
+	if err := DB.Where("id = ?", id).Find(&user).Error; err != nil {
+		return err
+	}
+	if name != "" {
+		user.Name = name
+	}
+	if email != "" {
+		user.Email = email
+	}
+	if phone != "" {
+		user.Phone = phone
+	}
+	if password != "" {
+		pw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			return err
+		}
+		user.Password = string(pw)
+	}
+	return DB.Save(&user).Error
 }
